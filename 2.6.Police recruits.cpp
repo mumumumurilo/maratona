80bc6fbdf13101149823e31070c88886edee51c7
mumumumurilo <130057461+mumumumurilo@users.noreply.github.com>
2023-05-05 08:57:09 -0300

    Add files via upload

---
+++ 2.6.Police recruits.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int n, policiais_livres=0, total=0, evento=0;
+    cin >> n;
+    for (int i=0; i<n; i++)
+    {
+        cin >> evento;
+        if (evento>0)
+        {
+            policiais_livres+=evento;
+        }
+        else
+        {
+            if (policiais_livres>0)
+            {
+                policiais_livres--;
+            }
+            else
+            {
+                total++;
+            }
+        }
+    }
+    cout << total;
+    return 0;
+}
